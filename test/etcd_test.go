1b29c5cf0d873f4174d6296697c8578c5ee077fc
qiankc <qiankc@isyscore.com>
2022-11-24 15:45:38 +0800

    test: etcd的测试函数

---
+++ test/etcd_test.go
@@ -0,0 +1,30 @@
+package test
+
+import (
+	"context"
+	"fmt"
+	"github.com/isyscore/isc-gobase/extend/etcd"
+	"github.com/isyscore/isc-gobase/isc"
+	"github.com/isyscore/isc-gobase/time"
+	"github.com/isyscore/isc-tracer/internal/trace"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+)
+
+func TestEtcd(t *testing.T) {
+	trace.OsTraceSwitch = true
+	trace.EtcdTraceSwitch = true
+
+	etcdClient, _ := etcd.NewEtcdClient()
+
+	ctx := context.Background()
+	etcdClient.Put(ctx, "test", time.TimeToStringYmdHms(time.Now()))
+
+	rsp, _ := etcdClient.Get(ctx, "test")
+	fmt.Println(rsp)
+
+	etcdClient.Get(ctx, "test", func(pOp *clientv3.Op) {
+		fmt.Println("信息")
+		fmt.Println(isc.ToJsonString(&pOp))
+	})
+}
